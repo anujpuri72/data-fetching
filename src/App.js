131847ec8d78138709b0e52910ae68a39eaa7e1c
Anuj Puri <anujpuri72@gmail.com>
2022-06-03 19:09:00 +0530

    Add basic data fetching code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+
+  const fetchBatchedData = async () => {
+    const arr = new Array(20).fill(0);
+
+    await arr.reduce(async (memo, _, index) => {
+      await memo;
+      const numbers = await Promise.allSettled(
+        new Array(5).fill(0).map((_, i) =>
+          axios
+            .get(
+              "https://jsonplaceholder.typicode.com/posts/" +
+                (index * 5 + (i + 1))
+            )
+            .then((res) => {
+              return res.data.id;
+            })
+        )
+      );
+      const ids = numbers.map((n) => n.value);
+      // console.log(index, ids);
+      setData((prev) => [...prev, ...ids]);
+    }, undefined);
+  };
+  // console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="button"
+        onClick={() => fetchBatchedData()}
+        disabled={data.length > 0}>
+        Click to start fetching
+      </button>
+      {data.map((element) => {
+        return <h1 key={element}>{element}</h1>;
+      })}
     </div>
   );
 }
-
-export default App;
